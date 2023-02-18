d2bf12342682232db47c2c66382c3506895dc7b3
Stepan Demchenko <stepan.demchenko@provenbase.com>
2023-02-18 20:16:32 +0200

    update end point

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  CONNECTION_URL: 'https://sheet.best/api/sheets/3d1121f2-bcbf-4df1-a1af-6d34312c7628'
+  CONNECTION_URL: 'https://sheet.best/api/sheets/3d8acac4-5199-461c-9dc4-8858c123d464'
 };
